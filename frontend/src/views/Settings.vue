a7b9034bbf0fcd4afeb174fa6e81738542547e52
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2025-09-07 20:59:04 +0300

    # prefix-unstable-version feat(added postgres) fix(fixed vue app routing)

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -3,27 +3,27 @@
         <h1 class="text-4xl font-bold">
             Settings
         </h1>
-        <p class="text-gray-400 text-base">Setup notifications and see other info</p>
+        <p class="text-gray-400 text-base">See info about this app</p>
     </div>
     <div class="flex flex-col w-full max-w- mx-auto p-2 mb-10">
-        <ul class="list bg-base-100 rounded-box shadow-md w-full relative hower:bg-base-200">
-            <li class="list-row items-center">
-                <i class="ri-github-fill text-2xl"></i>
-                <div>
-                    <div>Authors</div>
-                </div>
-                <a class="btn btn-square btn-ghost" href="https://github.com/sht0rmx/MiniAppTemplate">
+        <ul class="list bg-base-100 rounded-box shadow-md w-full relative hover:bg-base-200">
+            <li>
+                <a href="https://github.com/sht0rmx/TgMiniAppTemplate" target="_blank" rel="noopener noreferrer"
+                    class="list-row items-center flex w-full">
+                    <i class="ri-github-fill text-2xl"></i>
+                    <div class="flex-1">Authors</div>
                     <i class="ri-arrow-right-s-line"></i>
                 </a>
             </li>
         </ul>
+
     </div>
     <div class="text-center items-center text-sm text-gray-400 mt-5 space-y-2">
-        made with ❤️ by @sht0rmx        
+        made with ❤️ by @sht0rmx
     </div>
     <div class="text-center items-center text-sm text-gray-400 mt-2 space-x-2">
         <div class="badge badge-sm" :class="badgeDb" @click="fetchStatus();">{{ status }}</div>
-        <div class="badge badge-sm" :class="badgeAuth" @click="checkAuth();">{{ status_auth }}</div>       
+        <div class="badge badge-sm" :class="badgeAuth" @click="checkAuth();">{{ status_auth }}</div>
     </div>
 </template>
 
@@ -42,7 +42,7 @@ async function checkAuth() {
     if (ok) {
         const user = pb.authStore.record
         const uname = user?.username || user?.email || "unknown"
-        
+
         badgeAuth.value = "badge-success"
         status_auth.value = `auth as @${uname}`
     } else {
@@ -52,21 +52,21 @@ async function checkAuth() {
 }
 
 async function fetchStatus() {
-  try {
-    const result = await pb.health.check();
-    if (result.code !== 200) {
-      badgeDb.value = 'badge badge-error';
-      status.value = 'api unavailable';
-    } 
-    else {
-      badgeDb.value = 'badge badge-success';
-      status.value = "api is healthy"
+    try {
+        const result = await pb.health.check();
+        if (result.code !== 200) {
+            badgeDb.value = 'badge badge-error';
+            status.value = 'api unavailable';
+        }
+        else {
+            badgeDb.value = 'badge badge-success';
+            status.value = "api is healthy"
+        }
+    } catch (error) {
+        console.error('Failed to fetch db status:', error);
+        badgeDb.value = "badge-warning"
+        status.value = 'db unavailable';
     }
-  } catch (error) {
-    console.error('Failed to fetch db status:', error);
-    badgeDb.value = "badge-warning"
-    status.value = 'db unavailable';
-  }
 }
 
 fetchStatus();
--- pocketbase/pb_hooks/ping.pb.js
+++
@@ -1,5 +0,0 @@
-/// <reference path="../pb_data/types.d.ts" />
-
-routerAdd("GET", "/api/ping", (c) => {
-  return c.json(200, { pong: true })
-})
