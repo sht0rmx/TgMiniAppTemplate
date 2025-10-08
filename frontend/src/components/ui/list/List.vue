e54c1bbd50fe7d3cd22a46459df6a7e5e89a942c
m6rshm3ll0w <mihail.gm.kms@gmail.com>
2025-10-08 20:06:36 +0300

    added some edited shadcn-vue components

---
+++ frontend/src/components/ui/list/List.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Card } from '@/components/ui/card'
+
+const props = defineProps({
+  title: { type: String, required: false }
+})
+</script>
+
+<template>
+  <section class="space-y-1">
+    <h2 v-if="title" class="text-sm font-semibold tracking-wide text-muted-foreground">
+      {{ title }}
+    </h2>
+
+    <Card class="shadow-sm border rounded-2xl">
+      <div class="divide-y flex flex-col">
+        <slot />
+      </div>
+    </Card>
+  </section>
+</template>
